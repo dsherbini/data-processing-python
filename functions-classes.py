34b62612a009061157a16e50a4913a255843d54e
Danya Sherbini <danyasherbini@uchicago.edu>
2023-09-08 15:04:33 -0500

    Update functions-classes.py

--- functions-classes.py
+++ functions-classes.py
@@ -55,56 +55,7 @@ print(x) # still yields 40
 # function, making it more flexible and easier to use than if 'a' were hard-coded.
 
 
-
-# Question 3: Write a function that can generate a random password from
-# upper-case and lower-case letters, numbers, and special characters 
-# (!@#$%^&*).  It should have an argument for password length, and should 
-# check to make sure the length is between 8 and 16, or else warn the user 
-# and exit.  Your function should also have a keyword argument named 
-# "special_chars" that defaults to True.  If the function is called with the 
-# keyword argument set to False instead, then the random values chosen should
-# not include special characters.  Create a second similar keyword argument 
-# for numbers. Use one of the two libraries below.
-
-import random
-# from numpy import random
-
-# creating the function
-def pass_func(length, special = True, num = False): # adding in args and kwargs
-    length = int(length) # making length an integer
-    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' # defining letters
-    numbers = '123456789' # defining numbers
-    special_chars = '!@#$%^&*' # defining special characters
-    while True:
-        if length < 8 or length > 16: # if inputted length is less than 8 or more than 16
-            print("Sorry, too short.") # print this
-            break # and end the function
-        if length >= 8 or length <= 16: # if the length is 8-16 then
-            # apply these conditions depending on special and num arguments
-            if special == True and num == False:
-                alphabet = letters + special_chars
-                # .join joins together the random symbols chosen with random.choice()
-                password = ''.join(random.choice(alphabet) for i in range(length))
-            elif special == True and num == True:
-                alphabet = letters + special_chars + numbers
-                password = ''.join(random.choice(alphabet) for i in range(length))
-            elif special == False and num == True:
-                alphabet = letters + numbers
-                password = ''.join(random.choice(alphabet) for i in range(length))
-            elif special == False and num == False:
-                alphabet = letters
-                password = ''.join(random.choice(alphabet) for i in range(length))
-            return password
-
-
-# testing out the function
-pass_func(8) # with only the length argument + defaults
-pass_func(8, True, True) # with letters + special chars + numbers
-pass_func(12, False, True) # with only letters + numbers
-pass_func(16, False, False) # with only letters
-
-
-# Question 4: Create a class that requires four arguments when an instance
+# Question 3: Create a class that requires four arguments when an instance
 # is created: one for the person's name, one for which COVID vaccine they
 # have had, one for how many doses they've had, and one for whether they've
 # ever had COVID.  Then create instances for four people:
